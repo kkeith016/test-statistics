3bc9e9ae3014d57fb8b5bdc84602312b7dc42dbe
Kylee <kkeith.biz@gmail.com>
2025-10-02 14:10:40 -0400

    printing

--- src/main/java/com/pluralsight/TestStats.java
+++ src/main/java/com/pluralsight/TestStats.java
@@ -7,14 +7,34 @@ public class TestStats {
         then divide that sum by the total count of numbers in the set
          */
 
-        int [] scores = {85, 92, 76, 81, 90, 68, 95, 60, 73, 84};
+        int[] scores = {85, 92, 76, 81, 90, 68, 95, 60, 73, 84};
 
+        //Calculating average
         double sum = 0;
         for (int score : scores) {
             sum += score;
         }
         double average = sum / scores.length;
+
+        //Finding the high score and low score
+
+        int high = scores[scores.length - 1];
+        int low = scores[0];
+
+        for (int score : scores) {
+            if (score > high) {
+                high = score;
+            }
+            if (score > low) {
+                low = score;
+            }
+        }
+        System.out.println("-----Class Grades-----");
+        System.out.println("Highest score: " + high);
+        System.out.println("Lowest score: " + low);
+        System.out.println("Average score: " + average);
     }
+}
 
 
     /* creating a var named sum to store the total of all the score I choose to use a double
@@ -28,4 +48,4 @@ public class TestStats {
 
 
 
-}
+
